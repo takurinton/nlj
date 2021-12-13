8f62b18e7f739b405e75807596868f6c58d820c1
takurinton <takurinton@takurinton.com>
2021-12-13 21:59:09 +0900

    add: nlj

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
 def nlj():
-    print('nlj')
+    table1 = [
+        {
+            'username': 'takurinton',
+            'status': 1,
+        },
+        {
+            'username': 'hoge',
+            'status': 2,
+        },
+        {
+            'username': 'fuga',
+            'status': 2,
+        },
+        {
+            'username': 'piyo',
+            'status': 3,
+        }
+    ]
+
+    table2 = [
+        {
+            'status': 1,
+            'name': 'die',
+        },
+        {
+            'status': 2,
+            'name': 'fine',
+        },
+        {
+            'status': 3,
+            'name': 'tired',
+        }
+    ]
+
+    result = []
+    for t1 in table1:
+        for t2 in table2:
+            if t1['status'] == t2['status']:
+                result.append({
+                    'username': t1['username'],
+                    'status': t1['status'],
+                    'status_name': t2['name'],
+                })
+
+    print(result)
 
 if __name__ == '__main__':
     nlj()
